a13c5abfcea26f3402d72f478687b8f7f3171ba9
prashant1519 <prashantsingh19991215@gmail.com>
2023-10-03 22:18:58 +0530

    added string question

--- Array/Char.js
+++ Array/Char.js
@@ -1,3 +1,4 @@
+// git init
 // git add README.md
 // git commit -m "first commit"
 // git branch -M main
@@ -29,4 +30,59 @@
 // let f = r(str);
 // console.log(f);
 
-// reverse
+// duplicate
+// let arr = "madqam";
+// let s = 0;
+// let e = arr.length - 1;
+
+// for (let i = 0; i < arr.length; i++) {
+//   // const element = array[i];
+//   arr[2].pop();
+// }
+// console.log(arr);
+// function ss(arr) {
+//   while (s <= e) {
+//     if (arr[s] === arr[e]) {
+//       s++;
+//       e--;
+//     } else {
+//       //   return false;
+//       arr[s].pop();
+//     }
+//   }
+//   return true;
+// }
+
+// let r = ss(arr);
+// console.log(r);
+
+// function palin(arr) {
+//   while (s <= e) {
+//     if (arr[s] === arr[e]) {
+//       s++;
+//       e--;
+//     } else {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+let arr = "azxxzy";
+let s = 0;
+let e = arr.length - 1;
+let str = "";
+function ss(arr, str) {
+  while (s < e) {
+    if (str[str.length - 1] == arr[s]) {
+      str.slice(str.length - 1, arr[s]);
+    } else {
+      str = arr[s];
+    }
+    s++;
+  }
+  return str;
+}
+
+let r = ss(arr, str);
+console.log(r);
