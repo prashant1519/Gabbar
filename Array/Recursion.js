eb8fa578910589a975020704d564f9a48b73cf1e
prashant1519 <prashantsingh19991215@gmail.com>
2023-10-21 11:34:31 +0530

    sorted

--- Array/Recursion.js
+++ Array/Recursion.js
@@ -106,16 +106,94 @@
 // console.log(f);
 
 //printing array
-let arr = [10, 20, 30, 40, 50];
-function aq(n, size, i) {
+// let arr = [10, 20, 30, 40, 50];
+// function aq(n, size, i) {
+//   if (i >= size) {
+//     return;
+//   }
+
+//   console.log(n[i]);
+//   let g = aq(n, size, i + 1);
+//   return g;
+// }
+
+// let f = aq(arr, 5, 0);
+// console.log(f);
+
+//search in array
+// let arr = [10, 20, 25, 40, 50];
+// function aq(n, size, target, i) {
+//   if (i >= size) {
+//     return false;
+//   }
+
+//   if (n[i] == target) {
+//     return true;
+//   }
+
+//   let g = aq(n, size, target, i + 1);
+//   return g;
+// }
+
+// let f = aq(arr, 5, 20, 0);
+// console.log(f);
+
+//min in array
+// let arr = [42, 12, 10, 23, 21];
+// let str = [];
+// function aq(n, size, Max, i, str) {
+//   if (i >= size) {
+//     return;
+//   }
+
+//   if (n[i] < Max) {
+//     str.push[n[i]];
+//   }
+//   aq(n, size, Max, i + 1, str);
+
+//   for (let i = 0; i < str.length; i++) {
+//     console.log(str[i]);
+//   }
+// }
+
+// let f = aq(arr, 5, Infinity, 20, 0, str);
+// console.log(f);
+
+//double array
+
+// let arr = [10, 20, 30, 40, 50];
+// // let str = [];
+// function aq(n, size, i) {
+//   if (i >= size) {
+//     return;
+//   }
+//   n[i] = 2 * n[i];
+//   console.log(n[i]);
+//   aq(n, size, i + 1);
+// }
+
+// let f = aq(arr, 5, 0);
+// console.log(f);
+
+//sorting
+let arr = [10, 20, 30, 45, 50];
+function ra(arr, size, i) {
   if (i >= size) {
-    return;
+    return true;
   }
 
-  console.log(n[i]);
-  let g = aq(n, size, i + 1);
-  return g;
+  if (arr[i] > arr[i - 1]) {
+    let s = ra(arr, size, i + 1);
+    return s;
+  } else {
+    return false;
+  }
 }
 
-let f = aq(arr, 5, 0);
-console.log(f);
+let d = ra(arr, 5, 1);
+// console.log(d);
+if (d) {
+  console.log("sorted");
+} else {
+  console.log("not sorted");
+}
